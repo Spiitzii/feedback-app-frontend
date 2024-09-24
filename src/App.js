523e6b2ba98f3c1da3eab1ec3dd29b3429bcc854
Galina Atamankina <g.atamankina@gmail.com>
2024-09-24 12:51:58 +0200

    Adding feedback list and list item components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import FeedbackForm from './components/FeedbackForm';
+import FeedbackList from './components/FeedbackList';
+import { getFeedback } from './api/feedbackApi';
 
 function App() {
+  const [feedbacks, setFeedbacks] = useState([]);
+
+  useEffect(() => {
+      loadFeedback();
+  }, []);
+
+  const loadFeedback = async () => {
+    const feedbackData = await getFeedback();
+    setFeedbacks(feedbackData);
+  }
+
   return (
     <div>
       <h1>Feedback App</h1>
       <FeedbackForm />
+      <FeedbackList feedbacks={feedbacks}/>
     </div>
   );
 }
