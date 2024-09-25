1713c505eedaa39bcc45363765021ddcc7ceb605
Galina Atamankina <g.atamankina@gmail.com>
2024-09-25 10:35:14 +0200

    Adding the delete button to feedback items and the delete handler.

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { deleteFeedback } from "../api/feedbackApi";
 
 const FeedbackItem = ({feedback}) => {
+    
+    const handleDelete = async () => {
+        await deleteFeedback(feedback.title);
+    }
 
     return(
         <div className="feedback-item">
             <h3>{feedback.title}</h3>
             <p>{feedback.text}</p>
+            <button onClick={handleDelete}>Delete</button>
         </div>
     );
 };
--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 const BACKEND_HOST = process.env.FEEDBACK_BACKEND_HOST || "127.0.0.1";
-const BACKEND_PORT = process.env.FEEDBACK_BACKEND_PORT || 51815;
+const BACKEND_PORT = process.env.FEEDBACK_BACKEND_PORT || 57413;
 
 export const BACKEND_URL = `http://${BACKEND_HOST}:${BACKEND_PORT}/feedback`;
