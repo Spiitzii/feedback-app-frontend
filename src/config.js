1ad154efbea09e24b87f86a4f37e7a4ae0c2a6b3
Galina Atamankina <g.atamankina@gmail.com>
2024-09-25 17:00:49 +0200

    Test.

--- src/config.js
+++ src/config.js
@@ -1,4 +1,1 @@
-const BACKEND_HOST = process.env.REACT_APP_BACKEND_HOST;
-const BACKEND_PORT = process.env.REACT_APP_BACKEND_PORT;
-
-export const BACKEND_URL = `http://${BACKEND_HOST}:${BACKEND_PORT}/feedback`;
+export const BACKEND_URL = `/feedback`;
