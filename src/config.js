eb97d2439294381ff0c8e5de81b4700f3c23a602
Galina Atamankina <g.atamankina@gmail.com>
2024-09-26 09:27:29 +0200

    Revert "Test."
    This reverts commit 1ad154efbea09e24b87f86a4f37e7a4ae0c2a6b3.

--- src/config.js
+++ src/config.js
@@ -1,1 +1,4 @@
-export const BACKEND_URL = `/feedback`;
+const BACKEND_HOST = process.env.REACT_APP_BACKEND_HOST;
+const BACKEND_PORT = process.env.REACT_APP_BACKEND_PORT;
+
+export const BACKEND_URL = `http://${BACKEND_HOST}:${BACKEND_PORT}/feedback`;
