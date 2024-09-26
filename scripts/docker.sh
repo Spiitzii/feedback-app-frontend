545aa1d0989647647c35117a529ab233b5074b42
Galina Atamankina <g.atamankina@gmail.com>
2024-09-26 12:02:01 +0200

    Adding frontent kubernetes configs.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,12 +1,12 @@
 # Build the frontend app container
 docker build \
-    -t feedback-app-frontend:v1.0 \
+    -t feedback-app-frontend:v3.0 \
     -t feedback-app-frontend:latest \
-    -t galaataman/feedback-app-frontend:v1.0 \
+    -t galaataman/feedback-app-frontend:v3.0 \
     -t galaataman/feedback-app-frontend:latest .
 
 # Push the images to Docker Hub
-docker push galaataman/feedback-app-frontend:v1.0
+docker push galaataman/feedback-app-frontend:v3.0
 docker push galaataman/feedback-app-frontend:latest
 
 # Start the app with Docker Compose
